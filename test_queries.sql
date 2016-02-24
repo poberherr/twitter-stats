5e78be1860cd9cc3ba9681f736e536d10ba18e17
Patrick Oberherr <patrick.oberherr@gmail.com>
2016-02-24 15:11:51 +0100

    Adding test queries

--- test_queries.sql
+++ test_queries.sql
@@ -12,3 +12,43 @@ select count(distinct twitter_follower_id) from followers where twitter_id in
 (
 	select twitter_follower_id from followers where twitter_id = 237347211
 )
+
+
+
+
+-- followers_of_follower
+select count(distinct twitter_follower_id) from followers where twitter_id in
+(
+	select twitter_follower_id from followers where twitter_id = 237347211
+)
+
+select cast(created_at as date) as foo, favorite_count, is_retweet, retweet_count
+from tweets
+where user_id = 279
+group by foo, favorite_count, is_retweet, retweet_count
+order by foo
+
+
+
+
+
+
+select twitter_follower_id from followers where twitter_id = 48399689
+
+
+select * from users where id = 245;
+
+select * from followers where user_id = 244;
+
+select * from users where twitter_id = 4584605573;
+
+select * from users where screen_name = 'rlsux';
+
+select * from users where screen_name = 'nellykfm';
+
+
+select * from tweets where user_id = 279;
+
+select avg(is_retweet::int) from tweets where user_id = 279
+
+select coalesce(count(case when in_reply_to_user_id = null then 1 end)/count(*), 0) * 100 Percentage from tweets where user_id = 279
