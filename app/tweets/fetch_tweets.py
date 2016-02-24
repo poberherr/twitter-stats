a894db974ff1daa9af8835ed4d59946e1bdeba2a
Patrick Oberherr <patrick.oberherr@gmail.com>
2016-02-24 19:20:20 +0100

    Tweets app refactoring

--- app/tweets/fetch_tweets.py
+++ app/tweets/fetch_tweets.py
@@ -3,15 +3,18 @@ from app.users.models import Users
 from app.tweets.models import Tweets
 
 
-def fetch_and_create_tweets_by_id(twitter_id):
-    print('Fetching tweets for twitter_id: ', twitter_id)
-    tweets_data = twitter_api.user_timeline(id=twitter_id, count=100)
-    user = Users.query.filter_by(twitter_id=twitter_id).first()
+def fetch_and_create_tweets(user):
+    print('Fetching tweets for twitter_id: ', user.twitter_id)
+    tweets_data = twitter_api.user_timeline(id=user.twitter_id, count=100)
     for tweet_data in tweets_data:
         tweet_exists = Tweets.query.filter_by(tweet_id=tweet_data.id).first()
         if not tweet_exists:
-            print('Adding tweets for twitter_id: ', twitter_id)
+            print('Adding tweets for twitter_id: ', user.twitter_id)
             tweet = Tweets(tweet_data, user.id)
             tweet.add(tweet)
         if tweet_exists:
-            print('Tweet ', tweet_exists.tweet_id, 'is already in the DB for user twitter_id:', twitter_id)
+            print('Tweet ', tweet_exists.tweet_id, \
+                'is already in the DB for user twitter_id:', user.twitter_id)
+    tweets = Tweets.query.filter_by(user_id=user.id).\
+        order_by(Tweets.created_at.desc()).limit(100)
+    return tweets
--- app/tweets/views.py
+++ app/tweets/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 from app.tweets.models import Tweets, TweetsSchema, db
 from app.users.models import Users
-from app.tweets.fetch_tweets import fetch_and_create_tweets_by_id
+from app.tweets.fetch_tweets import fetch_and_create_tweets
 from flask_restful import Api, Resource
 
 tweets = Blueprint('tweets', __name__)
@@ -12,7 +12,7 @@ class FetchTweetsById(Resource):
     def get(self, id):
         user = Users.query.get_or_404(id)
         if user:
-            tweets = fetch_and_create_tweets_by_id(user.twitter_id)
+            tweets = fetch_and_create_tweets(user)
             # TODO: create tweet output schema
             return
         return 'User doesn\'t exist', 404
@@ -22,7 +22,7 @@ class FetchTweetsByName(Resource):
     def get(self, screen_name):
         user = Users.query.filter_by(screen_name=screen_name).first()
         if user:
-            tweets = fetch_and_create_tweets_by_id(user.twitter_id)
+            tweets = fetch_and_create_tweets(user)
         # TODO: Manually create tweet schema here again :S
         return
 
